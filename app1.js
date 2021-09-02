9299e479487c044288b7041486542ab7f063566d
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-09-02 11:18:30 -0700

    Condensed all JS into 1 file

--- app1.js
+++ app1.js
@@ -7,11 +7,43 @@ let numberVariable = 8
 // Boolean
 let booleanVariable = true
 
-let userName = prompt('Please Enter your Name');
 
-if(userName.toLowerCase() == 'roger') {
-    document.write('Welcome Dark Lord ' + userName);
+
+function getUserName() {
+    let userName = prompt('Please Enter your Name');
+
+    if(userName.toLowerCase() == 'roger') {
+        document.write('Welcome Dark Lord ' + userName);
+    }
+    else {
+        document.write('<h2>' + userName + '</h2>')
+    }
+    
+}
+
+function dealinAbsolutes() {
+    let userAnswer = prompt('Do you deal in absolutes?');
+
+    if(userAnswer.toLowerCase() == 'no') {
+        let url = "https://starwarsblog.starwars.com/wp-content/uploads/2019/04/claudia-master-tall-v2.jpg"
+        document.write('<img src="' + url + '">')
+    }
+    else {
+        let url = "https://cdn.vox-cdn.com/thumbor/4NeV5hN3hsEwOLwgRWUmNeQwaVg=/0x0:1280x720/920x613/filters:focal(538x258:742x462):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67764969/raypark.0.jpg"
+        document.write('<img src="' + url + '">')
+    }
+}
+
+function lightsaberColor(){
+    userAnswer = prompt('Blue or Red?');
+
+if(userAnswer.toLowerCase() == 'blue') {
+    let url = "https://mcdn.wallpapersafari.com/medium/10/39/TwfsOy.jpg"
+    document.write('<img src="' + url + '">')
 }
 else {
-    document.write('<h2>' + userName + '</h2>')
+    let url = "https://tvovermind.com/wp-content/uploads/2018/09/like-blood-from-a-stone-750x422.jpg"
+    document.write('<img src="' + url + '">')
+}
+
 }
--- app3.js
+++ app3.js
@@ -1,10 +1,1 @@
-userAnswer = prompt('Blue or Red?');
 
-if(userAnswer.toLowerCase() == 'blue') {
-    let url = "https://mcdn.wallpapersafari.com/medium/10/39/TwfsOy.jpg"
-    document.write('<img src="' + url + '">')
-}
-else {
-    let url = "https://tvovermind.com/wp-content/uploads/2018/09/like-blood-from-a-stone-750x422.jpg"
-    document.write('<img src="' + url + '">')
-}
