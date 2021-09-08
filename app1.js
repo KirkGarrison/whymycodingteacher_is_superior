519bcd9def25dd4447af98a89c8eeba04cd902a1
Stryder <stryder@strangedreams.com>
2021-09-08 11:04:42 -0400

    Refactor function name and indent code.

--- app1.js
+++ app1.js
@@ -35,17 +35,17 @@ function dealInAbsolutes() {
     document.write(`<img src="{url}" alt="{alt}" />`)
 }
 
-function lightsaberColor(){
-    userAnswer = prompt('Blue or Red?');
+function lightSaberColor(){
+    let userAnswer = prompt('Blue or Red?');
 
-if(userAnswer.toLowerCase() === 'blue') {
-    let url = "https://mcdn.wallpapersafari.com/medium/10/39/TwfsOy.jpg"
-    document.write('<img src="' + url + '">')
-}
-else {
-    let url = "https://tvovermind.com/wp-content/uploads/2018/09/like-blood-from-a-stone-750x422.jpg"
-    document.write('<img src="' + url + '">')
-}
+    if(userAnswer.toLowerCase() === 'blue') {
+        let url = "https://mcdn.wallpapersafari.com/medium/10/39/TwfsOy.jpg"
+        document.write('<img src="' + url + '">')
+    }
+    else {
+        let url = "https://tvovermind.com/wp-content/uploads/2018/09/like-blood-from-a-stone-750x422.jpg"
+        document.write('<img src="' + url + '">')
+    }
 
 }
 
